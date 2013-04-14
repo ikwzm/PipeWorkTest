682fe7b855fdd74ab6d3a0a9f432be92c4634966
KAWAZOME Ichiro <ichiro_k@ca2.so-net.ne.jp>
2013-04-15 01:34:07 +0900

    AXI4_ADAPTER の検証環境を追加. ただし検証は未完成.

---
+++ src/test/vhdl/axi4_adapter/axi4_write_adapter_test_bench.vhd
@@ -0,0 +1,53 @@
+-----------------------------------------------------------------------------------
+--!     @file    aix4_write_adapter_test_bench.vhd
+--!     @brief   TEST BENCH for AXI4_WRITE_ADPATER
+--!     @version 0.0.1
+--!     @date    2013/4/14
+--!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
+-----------------------------------------------------------------------------------
+--
+--      Copyright (C) 2012,2013 Ichiro Kawazome
+--      All rights reserved.
+--
+--      Redistribution and use in source and binary forms, with or without
+--      modification, are permitted provided that the following conditions
+--      are met:
+--
+--        1. Redistributions of source code must retain the above copyright
+--           notice, this list of conditions and the following disclaimer.
+--
+--        2. Redistributions in binary form must reproduce the above copyright
+--           notice, this list of conditions and the following disclaimer in
+--           the documentation and/or other materials provided with the
+--           distribution.
+--
+--      THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+--      "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+--      LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+--      A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT
+--      OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+--      SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+--      LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+--      DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+--      THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT 
+--      (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+--      OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+--
+-----------------------------------------------------------------------------------
+library ieee;
+use     ieee.std_logic_1164.all;
+use     ieee.numeric_std.all;
+use     std.textio.all;
+library DUMMY_PLUG;
+use     DUMMY_PLUG.AXI4_TYPES.all;
+use     DUMMY_PLUG.AXI4_MODELS.AXI4_MASTER_PLAYER;
+use     DUMMY_PLUG.AXI4_MODELS.AXI4_SLAVE_PLAYER;
+use     DUMMY_PLUG.AXI4_MODELS.AXI4_SIGNAL_PRINTER;
+use     DUMMY_PLUG.SYNC.all;
+use     DUMMY_PLUG.CORE.MARCHAL;
+use     DUMMY_PLUG.CORE.REPORT_STATUS_TYPE;
+use     DUMMY_PLUG.CORE.REPORT_STATUS_VECTOR;
+use     DUMMY_PLUG.CORE.MARGE_REPORT_STATUS;
+library PIPEWORK;
+use     PIPEWORK.COMPONENTS.SDPRAM;
+
