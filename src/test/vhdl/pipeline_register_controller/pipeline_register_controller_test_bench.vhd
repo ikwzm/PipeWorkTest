871573d54bb2887d3806e750cb66ca04d3e8f21f
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2018-05-08 16:43:58 +0900

    [fix] PIPELINE_REGISTER_CONTROLLER_TEST_BENCH

--- src/test/vhdl/pipeline_register_controller/pipeline_register_controller_test_bench.vhd
+++ src/test/vhdl/pipeline_register_controller/pipeline_register_controller_test_bench.vhd
@@ -83,13 +83,14 @@ architecture MODEL of PIPELINE_REGISTER_CONTROLLER_TEST_BENCH is
     signal      I_DATA          :  std_logic_vector(7 downto 0);
     signal      I_VAL           :  std_logic;
     signal      I_RDY           :  std_logic;
-    signal      O_DATA          :  std_logic_vector(7 downto 0);
+    signal      Q_DATA          :  std_logic_vector(7 downto 0);
     signal      Q_VAL           :  std_logic;
     signal      Q_RDY           :  std_logic;
     signal      LOAD            :  std_logic_vector(QUEUE_SIZE downto 0);
     signal      SHIFT           :  std_logic_vector(QUEUE_SIZE downto 0);
     signal      VALID           :  std_logic_vector(QUEUE_SIZE downto 0);
-    signal      O_BUSY          :  boolean;
+    signal      I_RUN           :  boolean;
+    signal      O_RUN           :  boolean;
     function    MESSAGE_TAG return STRING is
     begin
         return "(QUEUE_SIZE="  & INTEGER_TO_STRING(QUEUE_SIZE) & "):";
@@ -122,7 +123,7 @@ begin
     --
     -------------------------------------------------------------------------------
     REGS_0: if (QUEUE_SIZE = 0) generate
-        O_DATA <= I_DATA;
+        Q_DATA <= I_DATA;
     end generate;
     -------------------------------------------------------------------------------
     --
@@ -130,12 +131,12 @@ begin
     REGS_1: if (QUEUE_SIZE = 1) generate
         process (CLK, RST) begin
             if (RST = '1') then
-                    O_DATA <= (others => '0');
+                    Q_DATA <= (others => '0');
             elsif (CLK'event and CLK = '1') then
                 if (CLR = '1') then
-                    O_DATA <= (others => '0');
+                    Q_DATA <= (others => '0');
                 elsif (LOAD(1) = '1') then
-                    O_DATA <= I_DATA;
+                    Q_DATA <= I_DATA;
                 end if;
             end if;
         end process;
@@ -145,28 +146,28 @@ begin
     -------------------------------------------------------------------------------
     REGS_2: if (QUEUE_SIZE > 1) generate
         type    DATA_VECTOR is array (integer range <>) of std_logic_vector(7 downto 0);
-        signal  Q_DATA      : DATA_VECTOR(QUEUE_SIZE+1 downto 1);
+        signal  QUEUE_DATA  : DATA_VECTOR(QUEUE_SIZE+1 downto 1);
     begin 
         process (CLK, RST) begin
             if (RST = '1') then
-                    Q_DATA <= (others => (others => '0'));
+                    QUEUE_DATA <= (others => (others => '0'));
             elsif (CLK'event and CLK = '1') then
                 if (CLR = '1') then
-                    Q_DATA <= (others => (others => '0'));
+                    QUEUE_DATA <= (others => (others => '0'));
                 else
                     for i in 1 to QUEUE_SIZE loop
                         if (LOAD(i) = '1') then
                             if (SHIFT(i) = '1') then
-                                Q_DATA(i) <= Q_DATA(i+1);
+                                QUEUE_DATA(i) <= QUEUE_DATA(i+1);
                             else
-                                Q_DATA(i) <= I_DATA;
+                                QUEUE_DATA(i) <= I_DATA;
                             end if;
                         end if;
                     end loop;
                 end if;
             end if;
         end process;
-        O_DATA <= Q_DATA(1);
+        Q_DATA <= QUEUE_DATA(1);
     end generate;
     -------------------------------------------------------------------------------
     --
@@ -184,7 +185,7 @@ begin
     --
     -------------------------------------------------------------------------------
     I_TEST: process
-        procedure I_SET(CNT: integer; DATA: integer) is
+        procedure TEST(CNT: integer; DATA: integer) is
         begin
             WAIT_CLK(CNT);
             I_VAL  <= '1';
@@ -193,8 +194,26 @@ begin
                 wait until (CLK'event and CLK = '1');
                 exit when  (I_RDY = '1');
             end loop;
-            I_VAL  <= '0';
-            I_DATA <= (others => '1');
+            I_VAL  <= '0' after DELAY;
+            I_DATA <= (others => '1') after DELAY;
+        end procedure;
+        procedure TEST_START is
+        begin
+            wait until (CLK'event and CLK = '1');
+            I_RUN <= TRUE after DELAY;
+            WAIT_LOOP: loop
+                wait until (CLK'event and CLK = '1');
+                exit when  (O_RUN = TRUE);
+            end loop;
+        end procedure;
+        procedure TEST_DONE is
+        begin
+            wait until (CLK'event and CLK = '1');
+            I_RUN <= FALSE after DELAY;
+            WAIT_LOOP: loop
+                wait until (CLK'event and CLK = '1');
+                exit when  (O_RUN = FALSE);
+            end loop;
         end procedure;
     begin 
         ---------------------------------------------------------------------------
@@ -206,46 +225,75 @@ begin
                               RST     <= '1';
                               I_VAL   <= '0';
                               I_DATA  <= (others => '1');
+                              I_RUN   <= FALSE;
         WAIT_CLK( 4);         RST     <= '0';
                               CLR     <= '0';
         WAIT_CLK( 4);
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 1.1
         ---------------------------------------------------------------------------
-        assert(false) report MESSAGE_TAG & "SCENARIO 1" severity NOTE;
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.1 Start" severity NOTE;
         for i in 1 to 10 loop
-            I_SET(0, i);
+            TEST(0, i);
         end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.1 Done"  severity NOTE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 1.2
         ---------------------------------------------------------------------------
-        assert(false) report MESSAGE_TAG & "SCENARIO 2" severity NOTE;
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.2 Start" severity NOTE;
+        for i in 11 to 20 loop
+            TEST(0, i);
+        end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.2 Done"  severity NOTE;
+        ---------------------------------------------------------------------------
+        -- SCENARIO 1.3
+        ---------------------------------------------------------------------------
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.3 Start" severity NOTE;
+        WAIT_CLK(4);
+        for i in 21 to 30 loop
+            TEST(0, i);
+        end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 1.3 Done"  severity NOTE;
+        ---------------------------------------------------------------------------
+        -- SCENARIO 2.1
+        ---------------------------------------------------------------------------
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.1 Start" severity NOTE;
         for i in 1 to 10 loop
-            I_SET(0, i);
+            TEST(0, i);
         end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.1 Done"  severity NOTE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 2.2
         ---------------------------------------------------------------------------
-        assert(false) report MESSAGE_TAG & "SCENARIO 3" severity NOTE;
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.2 Start" severity NOTE;
         for i in 1 to 10 loop
-            I_SET(1, i);
+            TEST(1, i);
         end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.2 Done"  severity NOTE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 2.3
         ---------------------------------------------------------------------------
-        assert(false) report MESSAGE_TAG & "SCENARIO 4" severity NOTE;
+        TEST_START;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.3 Start" severity NOTE;
         for i in 1 to 10 loop
-            I_SET(1, i);
+            TEST(1, i);
         end loop;
+        TEST_DONE;
+        assert(false) report MESSAGE_TAG & "SCENARIO 2.3 Done"  severity NOTE;
         ---------------------------------------------------------------------------
         -- シミュレーション終了
         ---------------------------------------------------------------------------
         WAIT_CLK(10);
-        assert(false) report MESSAGE_TAG & "Wait O_TEST..." severity NOTE;
-        WAIT_LOOP: loop
-            wait until (CLK'event and CLK = '1');
-            exit when  (O_BUSY = FALSE);
-        end loop;
         assert(false) report MESSAGE_TAG & "Run complete..." severity NOTE;
         FINISH  <= 'Z';
         clk_ena <= FALSE;
@@ -255,51 +303,95 @@ begin
     --
     -------------------------------------------------------------------------------
     O_TEST: process
-        procedure O_CHK(CNT: integer; DATA: integer) is
+        procedure TEST(CNT: integer; DATA: integer) is
             variable ext_data : std_logic_vector(7 downto 0);
+            variable int_data : integer;
         begin
             ext_data := std_logic_vector(to_unsigned(DATA,8));
             WAIT_CLK(CNT);
             Q_RDY  <= '1';
             WAIT_LOOP: loop
                 wait until (CLK'event and CLK = '1');
-                exit when  (I_VAL = '1');
+                exit when  (Q_VAL = '1');
             end loop;
-            Q_RDY  <= '0';
-            assert (I_DATA = ext_data) report MESSAGE_TAG & "Mismatch O_DATA" severity ERROR;
+            int_data := to_integer(unsigned(Q_DATA));
+            assert (Q_DATA = ext_data) report MESSAGE_TAG & "Mismatch O_DATA=" & INTEGER_TO_STRING(int_data) & " EXT_DATA=" & INTEGER_TO_STRING(DATA) severity ERROR;
+            Q_RDY  <= '0'  after DELAY;
+        end procedure;
+        procedure TEST_START is
+        begin
+            wait until (CLK'event and CLK = '1');
+            O_RUN <= TRUE after DELAY;
+            WAIT_LOOP: loop
+                wait until (CLK'event and CLK = '1');
+                exit when  (I_RUN = TRUE);
+            end loop;
+        end procedure;
+        procedure TEST_DONE is
+        begin
+            wait until (CLK'event and CLK = '1');
+            O_RUN <= FALSE after DELAY;
+            WAIT_LOOP: loop
+                wait until (CLK'event and CLK = '1');
+                exit when  (I_RUN = FALSE);
+            end loop;
         end procedure;
     begin
-        O_BUSY <= TRUE;
         Q_RDY  <= '0';
+        O_RUN  <= FALSE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 1.1
         ---------------------------------------------------------------------------
+        TEST_START;
         for i in 1 to 10 loop
-            O_CHK(0, i);
+            TEST(0, i);
         end loop;
+        TEST_DONE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 1.2
         ---------------------------------------------------------------------------
+        TEST_START;
+        WAIT_CLK(3);
+        for i in 11 to 20 loop
+            TEST(0, i);
+        end loop;
+        TEST_DONE;
+        ---------------------------------------------------------------------------
+        -- SCENARIO 1.3
+        ---------------------------------------------------------------------------
+        TEST_START;
+        for i in 21 to 30 loop
+            TEST(0, i);
+        end loop;
+        TEST_DONE;
+        ---------------------------------------------------------------------------
+        -- SCENARIO 2.1
+        ---------------------------------------------------------------------------
+        TEST_START;
         for i in 1 to 10 loop
-            O_CHK(1, i);
+            TEST(1, i);
         end loop;
+        TEST_DONE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 2.2
         ---------------------------------------------------------------------------
+        TEST_START;
         for i in 1 to 10 loop
-            O_CHK(0, i);
+            TEST(0, i);
         end loop;
+        TEST_DONE;
         ---------------------------------------------------------------------------
-        -- 
+        -- SCENARIO 2.3
         ---------------------------------------------------------------------------
+        TEST_START;
         for i in 1 to 10 loop
-            O_CHK(1, i);
+            TEST(1, i);
         end loop;
+        TEST_DONE;
         ---------------------------------------------------------------------------
         -- シミュレーション終了
         ---------------------------------------------------------------------------
         assert(false) report MESSAGE_TAG & "Complete O_TEST..." severity NOTE;
-        O_BUSY <= FALSE;
         wait;
     end process;
 end MODEL;
