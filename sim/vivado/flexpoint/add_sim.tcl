6f033d31529468903133da78b5cd03dbb3131366
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2020-03-19 22:57:07 +0900

    [add] sim/vivado/flexpoint

---
+++ sim/vivado/flexpoint/add_sim.tcl
@@ -0,0 +1,3 @@
+add_vhdl_file sim_1     DUMMY_PLUG ../../../Dummy_Plug/src/main/vhdl/core/util.vhd
+add_vhdl_file sources_1 PIPEWORK ../../../PipeWork/src/flexpoint/flex_point_types.vhd
+add_vhdl_file sim_1     WORK ../../../src/test/vhdl/flexpoint/test_bench.vhd
