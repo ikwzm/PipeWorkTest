c22db60543fdf108cb49d57f651c998306b85447
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2023-12-22 16:10:22 +0900

    [add] sim/vivado/justifier

---
+++ sim/vivado/justifier/add_sources.tcl
@@ -0,0 +1,3 @@
+add_vhdl_file sources_1 PIPEWORK ../../../PipeWork/src/components/components.vhd
+add_vhdl_file sources_1 PIPEWORK ../../../PipeWork/src/components/justifier.vhd
+add_vhdl_file sources_1 WORK ../../../src/test/vhdl/justifier/justifier_test_bench.vhd
