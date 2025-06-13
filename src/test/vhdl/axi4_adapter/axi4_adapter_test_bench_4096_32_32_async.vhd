c741d4a7df9bc58f2e98d10955ee6480af7659db
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-06-13 12:05:08 +0900

    [add] T_CLK_FLOP and M_CLK_FLOP generic to AXI4_ADAPTER_TEST_BENCH

--- src/test/vhdl/axi4_adapter/axi4_adapter_test_bench_4096_32_32_async.vhd
+++ src/test/vhdl/axi4_adapter/axi4_adapter_test_bench_4096_32_32_async.vhd
@@ -1,12 +1,12 @@
 -----------------------------------------------------------------------------------
 --!     @file    aix4_adapter_test_bench_4096_32_32_async.vhd
 --!     @brief   AXI4_ADPATER TEST BENCH(MAX_XFER_SIZE=4096,T_DATA_WIDTH=32,M_DATA_WIDTH=32)
---!     @version 2.2.0
---!     @date    2024/4/7
+--!     @version 2.4.0
+--!     @date    2025/6/12
 --!     @author  Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
 -----------------------------------------------------------------------------------
 --
---      Copyright (C) 2012-2024 Ichiro Kawazome
+--      Copyright (C) 2012-2025 Ichiro Kawazome
 --      All rights reserved.
 --
 --      Redistribution and use in source and binary forms, with or without
@@ -47,10 +47,12 @@ architecture MODEL of AXI4_ADAPTER_TEST_BENCH_4096_32_32_ASYNC is
 begin
     TB: AXI4_ADAPTER_TEST_BENCH generic map (
         NAME            => string'("AXI4_ADAPTER_TEST_BENCH_4096_32_32_ASYNC"),
-        SCENARIO_FILE   => string'("axi4_adapter_test_bench_4096_32_32.snr"),
+        SCENARIO_FILE   => string'("axi4_adapter_test_bench_4096_32_32_async.snr"),
         T_CLK_RATE      => 0,
+        T_CLK_FLOP      => 2,
         T_CLK_PERIOD    => 10 ns,
         M_CLK_RATE      => 0,
+        M_CLK_FLOP      => 2,
         M_CLK_PERIOD    => 10 ns,
         T_DATA_WIDTH    => 32,
         M_DATA_WIDTH    => 32,
