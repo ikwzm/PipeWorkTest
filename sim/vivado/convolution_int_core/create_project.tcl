51b5f0065d5888c116083791f0e4b3bd8b0c6b06
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-10-31 20:13:17 +0900

    [update] sim/vivado/convolution_int_core for vivado 2025.1

--- sim/vivado/convolution_int_core/create_project.tcl
+++ sim/vivado/convolution_int_core/create_project.tcl
@@ -77,8 +77,10 @@ source "add_sim.tcl"
 # Set 'sim_1' fileset properties
 #
 set current_vivado_version [version -short]
-if       { [string first "2019.2" $current_vivado_version ] == 0 } {
+if       { [string first "2025.1" $current_vivado_version ] == 0 } {
     set scenario_full_path [file join ".." ".." ".." ".." $scenario_file ]
+} elseif { [string first "2019.2" $current_vivado_version ] == 0 } {
+    set scenario_full_path [file join ".." ".." ".."      $scenario_file ]
 } elseif { [string first "2018.3" $current_vivado_version ] == 0 } {
     set scenario_full_path [file join ".." ".." ".."      $scenario_file ]
 } elseif { [string first "2017"   $current_vivado_version ] == 0 } {
